6d6d53b1aac99217bb2b10934297aac0d3ba22d1
Olexiy Sokurenko <ol.sakura@gmail.com>
2020-08-26 23:04:34 +0200

    Modularisation with namespaces

--- src/app.ts
+++ src/app.ts
@@ -3,127 +3,13 @@
 /* eslint-disable no-useless-constructor */
 /* eslint-disable lines-between-class-members */
 /* eslint-disable max-classes-per-file */
+/// <reference path="models/drag-and-drop.ts"/>
+/// <reference path="models/project.ts"/>
+/// <reference path="project-state.ts"/>
+/// <reference path="decorators/autobind.ts"/>
+/// <reference path="util/validation.ts"/>
 
-// Drag & Drop Interfaces
-interface Draggable{
-    dragStartHandler(event: DragEvent): void;
-    dragEndHandler(event: DragEvent): void;
-}
-
-interface DragTarget{
-    dragOverHandler(event: DragEvent): void;
-    dropHandler(event: DragEvent): void;
-    dragLeaveHandler(event: DragEvent): void;
-
-}
-// Project status enumeration
-enum ProjectStatus{
-    Active, Finished
-}
-// Project Type
-class Project {
-  constructor(public id: string,
-                public title: string,
-                public description: string,
-                public people: number,
-                public status: ProjectStatus) {}
-}
-// Project State Management
-type Listener<T> = (items: T[]) => void;
-
-class State<T> {
-    protected listeners: Listener<T>[] = [];
-    addListener(listenerFn: Listener<T>) {
-      this.listeners.push(listenerFn);
-    }
-}
-
-class ProjectState extends State<Project> {
-    private projects: Project[] = [];
-    private static instance: ProjectState;
-
-    private constructor() {
-      super();
-    }
-    static getInstance() {
-      if (this.instance) {
-        return this.instance;
-      }
-      this.instance = new ProjectState();
-      return this.instance;
-    }
-
-    addProject(title: string, description:string, numOfPeople: number) {
-      const newProject = new Project(
-        Math.random().toString(),
-        title,
-        description,
-        numOfPeople,
-        ProjectStatus.Active,
-      );
-      this.projects.push(newProject);
-      this.updateListeners();
-    }
-
-    moveProject(projectId: string, newStatus: ProjectStatus) {
-      const project = this.projects.find((prj) => prj.id === projectId);
-      if (project && project.status !== newStatus) {
-        project.status = newStatus;
-      }
-      this.updateListeners();
-    }
-
-    private updateListeners() {
-      for (const listenerFn of this.listeners) {
-        listenerFn(this.projects.slice());
-      }
-    }
-}
-
-const projectState = ProjectState.getInstance();
-
-// autobind decorator
-function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
-  const originalMethod = descriptor.value;
-  const adjDescriptor: PropertyDescriptor = {
-    configurable: true,
-    get() {
-      const boundFn = originalMethod.bind(this);
-      return boundFn;
-    },
-  };
-  return adjDescriptor;
-}
-
-// validation logic
-interface Validatable {
-    value: string|number;
-    required?: boolean;
-    minLength?: number;
-    maxLength?: number;
-    min?: number;
-    max?: number;
-}
-
-function validate(validatableInput: Validatable) {
-  let isValid = true;
-  if (validatableInput.required) {
-    isValid = isValid && validatableInput.value.toString().trim().length !== 0;
-  }
-  if (validatableInput.minLength != null && typeof validatableInput.value === 'string') {
-    isValid = isValid && validatableInput.value.length >= validatableInput.minLength;
-  }
-  if (validatableInput.maxLength != null && typeof validatableInput.value === 'string') {
-    isValid = isValid && validatableInput.value.length <= validatableInput.maxLength;
-  }
-  if (validatableInput.min != null && typeof validatableInput.value === 'number') {
-    isValid = isValid && validatableInput.value >= validatableInput.min;
-  }
-  if (validatableInput.max != null && typeof validatableInput.value === 'number') {
-    isValid = isValid && validatableInput.value <= validatableInput.max;
-  }
-  return isValid;
-}
+namespace App{
 
 // Component Base Class
 abstract class Component<T extends HTMLElement, U extends HTMLElement> {
@@ -314,6 +200,7 @@ class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
     renderContent() {}
 }
 
-const prjInput = new ProjectInput();
-const activeProjectList = new ProjectList('active');
-const finishedProjectList = new ProjectList('finished');
+    new ProjectInput();
+    new ProjectList('active');
+    new ProjectList('finished');
+}
---
+++ src/models/project.ts
@@ -0,0 +1,14 @@
+namespace App{
+    // Project status enumeration
+    export enum ProjectStatus{
+    Active, Finished
+    }
+    // Project Type
+    export class Project {
+      constructor(public id: string,
+                public title: string,
+                public description: string,
+                public people: number,
+                public status: ProjectStatus) {}
+    }
+}
